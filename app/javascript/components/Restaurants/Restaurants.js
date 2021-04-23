5ebf8da7e2eb670951c6bfc658fb56547fc14581
PiperS52 <simon.a.piper87@gmail.com>
2021-04-23 22:05:59 +0100

    added all rests get call

--- app/javascript/components/Restaurants/Restaurants.js
+++ app/javascript/components/Restaurants/Restaurants.js
@@ -1,7 +1,29 @@
-import React from 'react' 
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios' 
 
 const Restaurants = () => {
-  return <div>This is the Restaurants#index view for the app</div>
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    //get all restaurants from api & then update restaurants in state
+
+    axios.get('/api/v1/restaurants.json')
+    .then( resp => {
+      setRestaurants(resp.data.data)
+    })
+    .catch( resp => console.log(resp))
+  }, [restaurants.length])
+
+  const list = restaurants.map( item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
+  return (
+    <Fragment>
+      <div>This is the Restaurants#index view for the app</div>
+      <ul>{list}</ul>
+    </Fragment>
+  )
 }
 
 export default Restaurants
