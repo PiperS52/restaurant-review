1bd06069c5108535dbfc2ea83c89d1e6d3261535
PiperS52 <simon.a.piper87@gmail.com>
2021-04-24 18:02:00 +0100

    added grid with styled-components

--- app/javascript/components/Restaurants/Restaurants.js
+++ app/javascript/components/Restaurants/Restaurants.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios' 
+import Restaurant from './Restaurant' 
+import styled from 'styled-components'
 
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1{
+    font-size: 42px;
+  }
+`
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+` 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
 const Restaurants = () => {
   const [restaurants, setRestaurants] = useState([])
 
@@ -8,21 +34,29 @@ const Restaurants = () => {
     //get all restaurants from api & then update restaurants in state
 
     axios.get('/api/v1/restaurants.json')
-    .then( resp => {
-      setRestaurants(resp.data.data)
-    })
+    .then( resp => setRestaurants(resp.data.data))
     .catch( resp => console.log(resp))
   }, [restaurants.length])
 
-  const list = restaurants.map( item => {
-    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  const grid = restaurants.map( item => {
+    return (
+      <Restaurant 
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
   })
 
   return (
-    <Fragment>
-      <div>This is the Restaurants#index view for the app</div>
-      <ul>{list}</ul>
-    </Fragment>
+    <Home>
+      <Header>
+        <h1>Restaurant Reviews</h1>
+        <Subheader>Honest, unbiased restaurant reveiws</Subheader>
+      </Header>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
   )
 }
 
